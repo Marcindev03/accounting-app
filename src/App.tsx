bc40bbb07db32ddace7a40a3975a2b18b19c9274
Marcindev03 <marcindev03@gmail.com>
2021-07-04 15:42:39 +0200

    create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <main></main>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="page-footer transparent">
+    <div className="footer-copyright grey darken-3">
+      <div className="container center white-text">
+        Copyright &copy; 2021 Marcin Sacha
+        <span>
+          <a
+            href="https://github.com/Marcindev03"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            Github
+          </a>
+        </span>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
